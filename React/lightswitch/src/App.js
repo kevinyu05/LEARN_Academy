b4aa518dadf78dd95b938c4393c29453fa09fc4f
Kevin Yu <kevinyu05@gmail.com>
2019-02-17 21:57:40 -0800

    updated commit

---
+++ React/lightswitch/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Light from './light.js'
+import Switch from './switch.js'
+import Buttons from './buttons.js'
+import Header from './header.js'
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      lightArray :[<Light />]
+    }
+  }
+
+  addLight(){
+    let {lightArray} = this.state
+
+    lightArray.push(<Light/>)
+    this.setState({lightArray: lightArray})
+  }
+
+  removeLight(){
+    let {lightArray} = this.state
+    if (lightArray.length > 1){
+    lightArray.pop()}
+    this.setState({lightArray: lightArray})
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Header/>
+          <Buttons callAddLightsFunction={this.addLight.bind(this)} callRemoveLightsFunction={this.removeLight.bind(this)}/>
+        </div>
+        <div id="box">
+        {this.state.lightArray}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
